7750db159eb8962f6a98a425925abf8909c8f946
ASYRANI SYAZWAN BIN YUHANIS <93539057+Asyeerrr@users.noreply.github.com>
2025-05-30 01:04:16 +0800

    Update header.php
    Created the header, all dummy links for now

--- public/header.php
+++ public/header.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My App</title>
-	<link rel="stylesheet" href="/css/style.css">
-	<script src="/js/main.js" defer></script>
+	<title>MMU Talent Showcase Portal</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="js/main.js" defer></script>
 </head>
 <body>
 	<header>
-		<h1>My App</h1>
 		<nav>
-			<a href="/login">Login</a> | <a href="/register">Register</a>
+			<div class="navbar">
+				<div class="navbar-left">
+					<h1>Website Name</h1>
+					<a href="/catalogue">Catalogue</a> 
+					<a href="/forum">Forum</a>
+					<a href="/leaderboard">Leaderboard</a> 
+					<a href="/feedback">Feedback</a>
+					<a href="/announcement">Announcement</a> 
+					<a href="/faq">FAQ</a>
+				</div>
+
+				<div class="navbar-right">
+					<img src="images/search.png" class="navbar-img" alt="search">
+					<a href="#"><img src="images/bag.png" class="navbar-img" alt="bag"></a>
+					<a href="#"><img src="images/profile.png" class="navbar-prof" alt="profile"></a>
+				</div>
+			</div>
 		</nav>
 	</header>
