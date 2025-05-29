654080f22e3aca3dffda33948be4de80a6b044c3
ASYRANI SYAZWAN BIN YUHANIS <93539057+Asyeerrr@users.noreply.github.com>
2025-05-30 01:02:05 +0800

    Update footer.php
    created the footer, ada space untuk insert something like a feature or apa

--- public/footer.php
+++ public/footer.php
@@ -1,4 +1,19 @@
+<link rel="stylesheet" href="css/styles.css">
+
 	<footer>
+		<div class="footer-container">
+			<div class="footer-left">
+				<p>TATAU NAK LETAK APA</p>
+			</div>
+			<div class="footer-right">
+				<a href="/catalogue">Catalogue</a> 
+				<a href="/forum">Forum</a>
+				<a href="/leaderboard">Leaderboard</a> 
+				<a href="/feedback">Feedback</a>
+				<a href="/announcement">Announcement</a> 
+				<a href="/faq">FAQ</a>
+			</div>
+		</div>
 		<p>&copy; <?= date('Y') ?> My App. All rights reserved.</p>
 	</footer>
 </body>
