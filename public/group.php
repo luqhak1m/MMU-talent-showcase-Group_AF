eaf7652959d2689aaf7642255703eff597224879
ASYRANI SYAZWAN BIN YUHANIS <93539057+Asyeerrr@users.noreply.github.com>
2025-05-30 01:12:02 +0800

    Created group.php
    Contains the details of each group members

---
+++ public/group.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group Members</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/main.js"></script>
+</head>
+<body class="group-body">
+    <?php include("header.php") ?>
+
+    <div class="group-container">
+        <div class="group-1">
+            <figure>
+                <img src="images/profile.png" alt="Luqman Hakim Bin Noorazmi">
+            </figure>
+            <p>Name: Luqman Hakim Bin Noorazmi</p>
+            <p>Student ID: 1211101583</p>
+            <p>Section: TC1L/TT4L</p>
+            <p>Contact: <a href="mailto:1211101583@student.mmu.edu.my">1211101583@student.mmu.edu.my</a></p>
+        </div>
+        <div class="group-2">
+            <figure>
+                <img src="images/profile.png" alt="Tengku Alyssa Sabrina Binti Tengku Erwin">
+            </figure>
+            <p>Name: Tengku Alyssa Sabrina Binti Tengku Erwin</p>
+            <p>Student ID: 1211101069</p>
+            <p>Section: TC1L/TT1L</p>
+            <p>Contact: <a href="mailto:1211101069@student.mmu.edu.my">1211101069@student.mmu.edu.my</a></p>
+        </div>
+        <div class="group-3">
+            <figure>
+                <img src="images/profile.png" alt="Vaarindran A/L Nyanasegran">
+            </figure>
+            <p>Name: Vaarindran A/L Nyanasegran</p>
+            <p>Student ID: 1211103144</p>
+            <p>Section: TC1L/TT1L</p>
+            <p>Contact: <a href="mailto:1211103144@student.mmu.edu.my">1211103144@student.mmu.edu.my</a></p>
+        </div>
+        <div class="group-4">
+            <figure>
+                <img src="images/profile.png" alt="Asyrani Syazwan Bin Yuhanis">
+            </figure>
+            <p>Name: Asyrani Syazwan Bin Yuhanis</p>
+            <p>Student ID: 1211103222</p>
+            <p>Section: TC1L/TT1L</p>
+            <p>Contact: <a href="mailto:1211103222@student.mmu.edu.my">1211103222@student.mmu.edu.my</a></p>
+        </div>
+    </div>
+
+    <?php include("footer.php") ?>
+
+</body>
+</html>
