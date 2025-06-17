cb81b66eb8e957d2b0810600d834a803f0a8b740
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2025-06-17 16:05:27 +0800

    Removed /uploads files

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -142,21 +142,21 @@ class UserController {
         exit;
     }
 
-    public function logout(){
-        session_start(); 
-        session_unset();
-        session_destroy();
+    // public function logout(){
+    //     session_start(); 
+    //     session_unset();
+    //     session_destroy();
 
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params["path"], $params["domain"],
-                $params["secure"], $params["httponly"]
-            );
-        }
+    //     if (ini_get("session.use_cookies")) {
+    //         $params = session_get_cookie_params();
+    //         setcookie(session_name(), '', time() - 42000,
+    //             $params["path"], $params["domain"],
+    //             $params["secure"], $params["httponly"]
+    //         );
+    //     }
 
-        header("Location: /talent-portal/public/index.php"); 
-        exit;
-    }
+    //     header("Location: /talent-portal/public/index.php"); 
+    //     exit;
+    // }
 
 }
